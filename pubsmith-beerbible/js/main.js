c783f32f8c12c011a2e9a158ddd4c0cb030a9b70
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-07-16 13:35:22 -0500

    Delete pubsmith-beerbible directory

--- pubsmith-beerbible/js/main.js
+++
@@ -1,7 +0,0 @@
-const listItems = document.querySelectorAll('.popup');
-
-listItems.forEach(item => {
-    item.addEventListener('mouseleave', () => {
-        item.querySelector('.popup').style.display = 'none';
-    });
-});
